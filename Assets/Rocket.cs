9026893c13b6c91b65fd5a36af47b2552bab2e86
SjorsJessen <sjors.jessen@gmail.com>
2019-03-23 21:14:40 +0100

    Basic Input Rocket

---
+++ Assets/Rocket.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Thrusting");
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Rotating Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Rotating Right");
+        }
+    }
+}
